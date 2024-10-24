4bfbd3b6055dd1e59bfa77821310e971079dc3a7
RutvikPathak <rutvikpathak2000@gmail.com>
2024-10-24 14:06:40 -0400

    loading button (by Joshua)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,10 +22,7 @@ const Home = () => {
 						{!isLoading ? (
 							<TrafficMap exportOSMFileRef={mapRef} />
 						) : (
-							<button type="button" class="bg-indigo-500 ..." disabled>
-								<svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
-								Loading...
-							</button>
+							<div>Loading...</div>
 						)}
 					</div>
 				</div>
